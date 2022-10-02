0ee71d1ed38204310378eee0ba2a6d500cb7e2ef
Mykyta Kucherenko <phobos.nik@gmail.com>
2022-10-02 09:54:50 +0300

    Up versions

--- src/test/java/com/example/demo/model/service/impl/UserSvcImplTest.java
+++ src/test/java/com/example/demo/model/service/impl/UserSvcImplTest.java
@@ -3,31 +3,22 @@ package com.example.demo.model.service.impl;
 import com.example.demo.model.dao.entity.User;
 import com.example.demo.model.dto.request.CreateUserReq;
 import com.example.demo.model.service.UserSvc;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBeans;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Testcontainers
+@ActiveProfiles("docker")
 @SpringBootTest
 class UserSvcImplTest {
 
